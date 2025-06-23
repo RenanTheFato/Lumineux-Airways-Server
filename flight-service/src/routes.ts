da396201389cbd0a3fce6a906e22e8c2b6711068
RenanTheFato <renan.santana007@hotmail.com>
2025-06-23 11:00:02 -0300

    flight-service: setup flight micro-service

---
+++ flight-service/src/routes.ts
@@ -0,0 +1,8 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { FastifyTypedInstance } from "./@types/types.js";
+
+export async function routes(fastify: FastifyTypedInstance) {
+  fastify.get("/ping", async(req: FastifyRequest, rep: FastifyReply) => {
+    return rep.status(200).send({ message: "pong" })
+  })
+}
---
+++ flight-service/src/server.ts
@@ -0,0 +1,33 @@
+import { fastify } from "fastify";
+import { routes } from "./routes.js";
+import { jsonSchemaTransform, serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
+import cors from "@fastify/cors";
+import dotenv from "dotenv";
+
+
+dotenv.config()
+
+const server = fastify({ logger: true }).withTypeProvider<ZodTypeProvider>()
+
+async function start() {
+
+  const HOST = process.env.HTTP_HOST
+  const PORT = process.env.HTTP_PORT
+
+  server.setSerializerCompiler(serializerCompiler)
+  server.setValidatorCompiler(validatorCompiler)
+
+  await server.register(routes)
+
+  await server.listen({
+    host: HOST || '0.0.0.0',
+    port: Number(PORT) || 3334,
+  }).then(() => {
+    console.log(`HTTP server running on port: ${PORT}`)
+  }).catch((error) => {
+    console.error(`Error on trying running the HTTP server: ${error}`)
+    process.exit(1)
+  })
+}
+
+start()
